2ab9f33675ea2c11deedd3d877d035343989dead
Wouter Beek <me@wouterbeek.com>
2014-06-05 13:19:56 +0200

    Create an encapsulating Maven project:   - Easy dependency handling.   - Easy machine- and OS-independent deployment.

--- src/org/example/evoquery/Main.java
+++ src/org/example/evoquery/Main.java
@@ -1,11 +1,10 @@
-package main;
+package org.example.evoquery;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Random;
-
 
 import org.openrdf.query.BindingSet;
 
@@ -14,64 +13,70 @@ public class Main {
 	ArrayList<BindingSet> initialMemory;
 	public static RepositoryHandler rh;
 	public static HashSet<String> globalEntailments = new HashSet<String>();
-	
+
 	public Main() {
 		initializeRepository();
 	}
-	
+
 	private void initializeRepository() {
 		rh = new RepositoryHandler("repo");
-		initialMemory = rh.queryRepo("SELECT ?x ?p ?y WHERE { ?x ?p ?y } LIMIT 300000");
+		initialMemory = rh
+				.queryRepo("SELECT ?x ?p ?y WHERE { ?x ?p ?y } LIMIT 300000");
 		System.out.println("Initialized repository.");
 	}
-	
+
 	private ArrayList<BindingSet> randomMemory() {
 		ArrayList<BindingSet> result = new ArrayList<BindingSet>();
 		Random ra = new Random();
-		for(int i=0;i<100;i++)
+		for (int i = 0; i < 100; i++)
 			result.add(initialMemory.get(ra.nextInt(300000)));
 		return result;
 	}
-	
+
+	@SuppressWarnings("unused")
 	private void testAgent() {
 		Agent2 test = new Agent2(300, initialMemory);
-		for(int i=0;i<initialMemory.size();i++)
-			System.out.println(initialMemory.get(i).getValue("x")+" | "+initialMemory.get(i).getValue("p")+" | "+initialMemory.get(i).getValue("y"));
+		for (int i = 0; i < initialMemory.size(); i++)
+			System.out.println(initialMemory.get(i).getValue("x") + " | "
+					+ initialMemory.get(i).getValue("p") + " | "
+					+ initialMemory.get(i).getValue("y"));
 		test.buildQueries();
 		test.printQueries();
 	}
-	
+
 	private void initializePopulation() {
 		population = new Agent2[1000];
 		ArrayList<BindingSet> rm = randomMemory();
-		for(int i=0; i<population.length; i++) {
+		for (int i = 0; i < population.length; i++) {
 			population[i] = new Agent2(200, rm);
 		}
 		System.out.println("Initialized population.");
-		
+
 	}
-	
 
 	private void evolvePopulation(int n) {
 		Arrays.sort(population, new AgentTotalComparator());
-		
+
 		int top10score = 0;
 		int popscore = 0;
-		for(int i=0;i<10;i++)
+		for (int i = 0; i < 10; i++)
 			top10score += population[i].getTotalScore();
-		for(int i=0;i<population.length;i++)
+		for (int i = 0; i < population.length; i++)
 			popscore += population[i].getTotalScore();
-		System.out.println(n+". Topscorer: "+population[0].getTotalScore()+"   Top 10: "+top10score+"   Population: "+popscore);	
-		
-		for(int i=0;i<population.length/10;i++) {
-			population[population.length-i-1] = new Agent2(300, initialMemory, population[i].getParams(), population[i].getTermParams());
-			population[population.length-i-1].mutate();
+		System.out.println(n + ". Topscorer: " + population[0].getTotalScore()
+				+ "   Top 10: " + top10score + "   Population: " + popscore);
+
+		for (int i = 0; i < population.length / 10; i++) {
+			population[population.length - i - 1] = new Agent2(300,
+					initialMemory, population[i].getParams(),
+					population[i].getTermParams());
+			population[population.length - i - 1].mutate();
 		}
 		ArrayList<BindingSet> nm = randomMemory();
-		for(int i=0;i<population.length;i++) {
+		for (int i = 0; i < population.length; i++) {
 			population[i].resetRound(nm);
 		}
-		//System.out.println("Evolved population");
+		// System.out.println("Evolved population");
 	}
 
 	public static void main(String[] args) {
@@ -80,43 +85,40 @@ public class Main {
 		m.evolutionRounds(500, 5);
 
 	}
-	
+
 	public void evolutionRounds(int rounds, int steps) {
-		for(int i=0;i<rounds;i++) {
+		for (int i = 0; i < rounds; i++) {
 			takeSteps(steps);
 			evolvePopulation(i);
 		}
 		population[0].printQueries();
 	}
-	
+
 	public void testCode() {
-		ArrayList<BindingSet> ress = rh.queryRepo("SELECT ?x ?p ?y WHERE { <http://dbpedia.org/resource/Connecticut> ?p ?y } LIMIT 30");
-		for(int i=0;i<ress.size();i++)
-			System.out.println(ress.get(i).getValue("x")+" | "+ress.get(i).getValue("p")+" | "+ress.get(i).getValue("y"));
+		ArrayList<BindingSet> ress = rh
+				.queryRepo("SELECT ?x ?p ?y WHERE { <http://dbpedia.org/resource/Connecticut> ?p ?y } LIMIT 30");
+		for (int i = 0; i < ress.size(); i++)
+			System.out.println(ress.get(i).getValue("x") + " | "
+					+ ress.get(i).getValue("p") + " | "
+					+ ress.get(i).getValue("y"));
 	}
-	
-	
+
 	private void takeSteps(int n) {
-		for(int j=0;j<n;j++) {
+		for (int j = 0; j < n; j++) {
 			ArrayList<BindingSet> nm = randomMemory();
-			for(int i=0;i<population.length;i++) {
+			for (int i = 0; i < population.length; i++) {
 				population[i].takeStep(nm);
 			}
-			//System.out.println("Step taken.");		
+			// System.out.println("Step taken.");
 		}
-		
-		
+
 	}
 
-	private class AgentTotalComparator implements Comparator {
+	private class AgentTotalComparator implements Comparator<Agent2> {
 
-		public int agentCompare(Agent2 o1, Agent2 o2) {
-			return (o1.getTotalScore()>o2.getTotalScore() ? -1 : (o1.getTotalScore()==o2.getTotalScore() ? 0 : 1));
-		}
-
-		@Override
-		public int compare(Object o1, Object o2) {
-			return agentCompare((Agent2)o1, (Agent2)o2);
+		public int compare(Agent2 o1, Agent2 o2) {
+			return (o1.getTotalScore() > o2.getTotalScore() ? -1 : (o1
+					.getTotalScore() == o2.getTotalScore() ? 0 : 1));
 		}
 	}
 }
--- src/org/example/evoquery/RepositoryHandler.java
+++ src/org/example/evoquery/RepositoryHandler.java
@@ -1,4 +1,5 @@
-package main;
+package org.example.evoquery;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,21 +18,23 @@ import org.openrdf.rio.RDFParseException;
 import org.openrdf.sail.inferencer.fc.ForwardChainingRDFSInferencer;
 import org.openrdf.sail.memory.MemoryStore;
 
-
 public class RepositoryHandler {
 	Repository repo;
 	RepositoryConnection con;
 
 	public RepositoryHandler(String repositoryPath) {
-		
+
 		File dataDir = new File(repositoryPath);
 		MemoryStore store = new MemoryStore(dataDir);
-		ForwardChainingRDFSInferencer inf = new ForwardChainingRDFSInferencer(store);
+		ForwardChainingRDFSInferencer inf = new ForwardChainingRDFSInferencer(
+				store);
 		repo = new SailRepository(inf);
 		ArrayList<BindingSet> ress = queryRepo("SELECT * WHERE { ?x ?p ?y } LIMIT 30");
-		for(int i=0;i<30;i++)
-			System.out.println(ress.get(i).getValue("x")+" | "+ress.get(i).getValue("p")+" |  "+ress.get(i).getValue("y"));
-		
+		for (int i = 0; i < 30; i++)
+			System.out.println(ress.get(i).getValue("x") + " | "
+					+ ress.get(i).getValue("p") + " |  "
+					+ ress.get(i).getValue("y"));
+
 		try {
 			repo.initialize();
 			con = repo.getConnection();
@@ -43,34 +46,35 @@ public class RepositoryHandler {
 	public ArrayList<BindingSet> queryRepo(String query) {
 		ArrayList<BindingSet> returnSet = new ArrayList<BindingSet>();
 		try {
-			  String queryString = query;
-			  
-			  TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
-			  //System.out.println(tupleQuery.getIncludeInferred());
-			  TupleQueryResult result = tupleQuery.evaluate();			
-			  
-			  try {
-				  while(result.hasNext()) {
+			String queryString = query;
+
+			TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL,
+					queryString);
+			// System.out.println(tupleQuery.getIncludeInferred());
+			TupleQueryResult result = tupleQuery.evaluate();
+
+			try {
+				while (result.hasNext()) {
 					returnSet.add(result.next());
-					//System.out.println(returnSet.get(returnSet.size()-1));
-				  }
-			  }
-			  finally {
-			      result.close();
-			  }
-		   } catch(OpenRDFException e) {
-			   e.printStackTrace();
-		   } finally {
-			   
-		   }
+					// System.out.println(returnSet.get(returnSet.size()-1));
+				}
+			} finally {
+				result.close();
+			}
+		} catch (OpenRDFException e) {
+			e.printStackTrace();
+		} finally {
+
+		}
 		return returnSet;
-		
+
 	}
-	
+
 	public void addFiletoRepo(String filepath) {
 		File file = new File(filepath);
 		try {
-			con.add(file, "http://example.org/dbpedia_ontology", RDFFormat.RDFXML);
+			con.add(file, "http://example.org/dbpedia_ontology",
+					RDFFormat.RDFXML);
 		} catch (RDFParseException e) {
 			e.printStackTrace();
 		} catch (RepositoryException e) {
